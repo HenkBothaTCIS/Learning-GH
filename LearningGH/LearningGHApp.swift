96729bba3dcc408e684aab87334287d4cc0c4142
Henk Botha <bothah@tcis.or.kr>
2023-01-01 16:00:22 +0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by TCIS on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
